18a57ed3a218093bd909df9184ec57da73e3e465
Lucas Madrigal <lucasmadrigalb@gmail.com>
2024-07-21 03:30:31 -0300

    first commit - con fetch incluido

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { getLatestGames } from "./lib/metacritic";
 
 export default function App() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    getLatestGames().then((data) => setGames(data));
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Pressable
+        onPress={() => {}}
+        style={({ pressed }) => ({ backgroundColor: pressed ? "blue" : "red" })}
+      >
+        <Text>Pressable</Text>
+      </Pressable>
     </View>
   );
 }
@@ -13,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#09f",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
